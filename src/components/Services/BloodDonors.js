198c5e296051e12f02a1802c2a1a6eadc047792a
Bokkemolla Mahesh <bmahesh1374@gmail.com>
2026-04-13 11:01:05 +0530

    services some ui is changed

--- src/components/Services/BloodDonors.js
+++ src/components/Services/BloodDonors.js
@@ -1,10 +1,10 @@
 import React from "react";
 import "../Styles/Services.css";
 
-import ap from "../../assets/bloodgroup.png";
-import ts from "../../assets/bloodgroup.png";
-import tn from "../../assets/bloodgroup.png";
-import ww from "../../assets/bloodgroup.png";
+import ap from "../../assets/ap.png";
+import ts from "../../assets/tg.png";
+import tn from "../../assets/tn.png";
+import ww from "../../assets/worldwide.png";
 
 function BloodDonors() {
   return (
@@ -17,22 +17,22 @@ function BloodDonors() {
 
           <div className="donor-grid">
             <div className="donor-card">
-              <h5>Andhra Pradesh</h5>
+              {/* <h5>Andhra Pradesh</h5> */}
               <img src={ap} alt="AP" />
             </div>
 
             <div className="donor-card">
-              <h5>Telangana</h5>
+              {/* <h5>Telangana</h5> */}
               <img src={ts} alt="TS" />
             </div>
 
             <div className="donor-card">
-              <h5>Tamil Nadu</h5>
+              {/* <h5>Tamil Nadu</h5> */}
               <img src={tn} alt="TN" />
             </div>
 
             <div className="donor-card">
-              <h5>World Wide</h5>
+              {/* <h5>World Wide</h5> */}
               <img src={ww} alt="WW" />
             </div>
           </div>
--- src/components/Services/GlobalNetwork.js
+++ src/components/Services/GlobalNetwork.js
@@ -9,17 +9,14 @@ import globalImg from "../../assets/global-img.png";
 import event1Image from "../../assets/Events1.png";
 import event2Image from "../../assets/Events3.png";
 
-
-
 const GlobalNetwork = () => {
-
   const events = [
     {
       image: event1Image,
       title: "HR EVENT",
       organizer: "K. Siva Krishna",
       speaker: "K. Rajasekhar",
-      eventmode:"In Person",
+      eventmode: "In Person",
       schedule: "Aug 20 - Aug 23",
       time: "10AM - 4PM",
       location: "Ameerpet",
@@ -29,7 +26,7 @@ const GlobalNetwork = () => {
       title: "HR EVENT",
       organizer: "K. Siva Krishna",
       speaker: "K. Rajasekhar",
-      eventmode:"Online",
+      eventmode: "Online",
       schedule: "Jun 20 - Jun 23",
       time: "10AM - 4PM",
       location: "Ameerpet",
@@ -39,62 +36,76 @@ const GlobalNetwork = () => {
   return (
     <>
       <section className="knowledge-wrapper" id="global">
-      <div className="services-name">
-        <h3>Services</h3>
-      </div>
+        <div className="services-name">
+          <h3>Services</h3>
+        </div>
         <div className="container knowledge-section">
           <div className="row align-items-center g-4">
             <div className="col-lg-6 col-12 text-center mb-4 mb-lg-0">
-            <div className="globalimg">
-              <img src={globalImg} alt= "img" className="img-fluid"/>
-            </div>  
+              <div className="globalimg">
+                <img src={globalImg} alt="img" className="img-fluid" />
+              </div>
             </div>
             <div className="col-lg-6 col-12 text-center text-lg-start">
               <h3 className="knowledge-title">GLOBAL NETWORKING</h3>
               <p className="knowledge-description">
-                We provide global networking solutions that help businesses connect,
-                collaborate, and scale across borders. With secure infrastructure and 
-                24/7 support, we help you establish a reliable network that grows 
-                with your business and ensures uninterrupted operations.
+                We provide global networking solutions that help businesses
+                connect, collaborate, and scale across borders. With secure
+                infrastructure and 24/7 support, we help you establish a
+                reliable network that grows with your business and ensures
+                uninterrupted operations.
               </p>
             </div>
           </div>
 
-           <div className="events-row">
-                    <div className="event-card">
-                    <div>
-                        <img src={event1Image} alt="event" />
-                        <h6>Sri Jetti Kusum Kumar</h6>
-                        <p>Founder of KGF</p>
-                    </div>
-                      
-                      <div className="event-content">
-                        <h3>KGF Global Event</h3>
-                        <p className="invite">You Are Invited!</p>
-                        <p><SlCalender /> Aug 20 - Aug 21 | <FaClock/> 10pm - 1am</p>
-                        <p><IoLocationOutline /> Venkata Function Hall, Hyderabad</p>
-                        <button className="register-btn">REGISTER NOW</button>
-                      </div>
-                    </div>
-          
-                    <div className="event-card">
-                      <div>
-                      <img src={event2Image} alt="event" />
-                      <h6>Sri Jetti Kusum Kumar</h6>
-                      </div>
-                      <div className="event-content">
-                        <h3>KGF Global Event</h3>
-                        <p className="invite">You Are Invited!</p>
-                        <p><SlCalender /> Aug 20 - Aug 21 | <FaClock/> 10pm - 1am</p>
-                        <p><IoLocationOutline /> Venkata Function Hall, Hyderabad</p>
-                        <button className="register-btn">REGISTER NOW</button>
-                      </div>
-                    </div>
-                  </div>
-          </div>
-      </section>
+          <div className="events-row">
+            <div className="event-card">
+              <div>
+                <img src={event1Image} alt="event" />
+                <h6>Sri Jetti Kusum Kumar</h6>
+                <p className="prof">Founder of KGF</p>
+              </div>
 
-    
+              <div className="event-content">
+                <h3>KGF Global Event</h3>
+                <p className="event-content-org">
+                  Organized by Sri JETTI KUSUM KUMAR
+                </p>
+                <p className="invite">You Are Invited!</p>
+                <p>
+                  <SlCalender /> Aug 20 - Aug 21 | <FaClock /> 10pm - 1am
+                </p>
+                <p>
+                  <IoLocationOutline /> Venkata Function Hall, Hyderabad-500029
+                </p>
+                <button className="register-btn">REGISTER NOW</button>
+              </div>
+            </div>
+
+            <div className="event-card">
+              <div>
+                <img src={event2Image} alt="event" />
+                <h6>M. Venkaiah Naidu</h6>
+                <p className="prof">Former Vice President of India</p>
+              </div>
+              <div className="event-content">
+                <h3>KGF Global Event</h3>
+                <p className="event-content-org">
+                  Organized by Sri JETTI KUSUM KUMAR
+                </p>
+                <p className="invite">You Are Invited!</p>
+                <p>
+                  <SlCalender /> Aug 20 - Aug 21 | <FaClock /> 10pm - 1am
+                </p>
+                <p>
+                  <IoLocationOutline /> Venkata Function Hall, Hyderabad-500029
+                </p>
+                <button className="completed-btn">COMPLETED</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
