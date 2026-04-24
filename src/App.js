35736394f8dab69537109c344b56da4a03c41f65
Bokkemolla Mahesh <bmahesh1374@gmail.com>
2026-04-24 10:51:22 +0530

    Conflits resolved

--- src/App.js
+++ src/App.js
@@ -6,39 +6,40 @@ import NavbarSection from "./components/Landing/NavbarSection";
 import LandingPage from "./components/Landing/Landingpage";
 import Routing from "./components/Routings/Routing";
 import ScrollToHash from "./components/Landing/ScrollToHash";
-
+ 
 import getInTouchImg from "./assets/getintouch.png";
 import Getintouch from "./components/Landing/Getintouch";
 import StudentMembership from "./components/Membership/StudentMembership";
 import Servicesgtregisterlanding from "./components/Register/Servicesgtregisterlanding";
-
+ 
 function App() {
   const [showPopup, setShowPopup] = useState(false);
-
+ 
   return (
     <div className="App">
       <NavbarSection />
       <Routing />
-
+ 
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/services" element={<Services />} />
       </Routes>
-
+ 
       <ScrollToHash />
-
+ 
       {/* <StudentMembership /> */}
-
+ 
       <div className="getintouch-img" onClick={() => setShowPopup(true)}>
         <img src={getInTouchImg} alt="Get In Touch" />
       </div>
-
+ 
       {showPopup && <Getintouch closePopup={() => setShowPopup(false)} />}
-
+ 
       {/* <Servicesgtregisterlanding /> */}
       {/* <StudentMembership /> */}
     </div>
   );
 }
-
+ 
 export default App;
+ 
