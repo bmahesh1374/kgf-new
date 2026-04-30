105d3924a1b3b273cfeadd80045a21b161839b0e
Surya Teja <suryateja@example.com>
2026-04-30 10:01:55 +0530

    Students Completed

--- src/components/AdminDashboard/AdminDashboardRoutes.js
+++ src/components/AdminDashboard/AdminDashboardRoutes.js
@@ -11,9 +11,7 @@ import Blood_Donate from "../Pages/Blood_Donate";
 import DEvents from "../Pages/DEvents";
 import ViewJob from "../Pages/ViewJob";
 import AdminDashboardHome from "./AdminDashboardHome";
-
 import AdminEvents from "../Pages/AdminEvents"
-
 import Telangana from "../Pages/Telangana";
 import AndhraPradesh from "../Pages/AndhraPradesh";
 import Gujarat from "../Pages/Gujarat";
@@ -23,6 +21,10 @@ import Organization from "../Pages/Organization";
 import OutsideOfIndia from "../Pages/OutsideOfIndia";
 import Organizations from "../Pages/Organizations";
 import Tamilnadu from "../Pages/Tamilnadu";
+import AdminStudentTable from "../Pages/AdminStudentTable";
+import AdminStTbleView from "../Pages/AdminStTbleView";
+
+
 
 
 
@@ -31,9 +33,7 @@ function AdminDashboardRoutes() {
   return (
     <Routes>
       <Route path="/admindashboard" element={<AdminDashboardHome/>} />
-      <Route path="/student" element={<Student />} /> 
       <Route path="/events" element={<AdminEvents />} />
-
       <Route path="/telangana" element={<Telangana/>} />
       <Route path="/andhra-pradesh" element={<AndhraPradesh/>} />
       <Route path="/gujarat" element={<Gujarat/>} />
@@ -43,8 +43,10 @@ function AdminDashboardRoutes() {
        <Route path="/outside-of-india" element={<OutsideOfIndia/>} />
        <Route path="/organizations" element={<Organizations/>} />
        <Route path="/tamil-nadu" element={<Tamilnadu/>} />
+       <Route path="/admin-student-table" element={<AdminStudentTable/>} />
+       <Route path="/AdminStTbleView" element={<AdminStTbleView/>}/>
 
-
+x
     </Routes>
   );
 }
